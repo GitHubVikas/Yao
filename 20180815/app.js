e24f976bd63ba0ca9f5bbabe1f22b2d0a8c6beb7
GitHubVikas <811822704@qq.com>
2018-09-18 09:46:43 +0800

    20180918

--- 20180815/app.js
+++ 20180815/app.js
@@ -1,11 +1,64 @@
-var time = new Date();
-time = time.toString();
-
+var time;
+var hours;
+var minutes;
+var secounds;
+var years;
+var months;
+var days;
+function getDays(){
+	time = new Date();
+	hours = time.getHours();
+	minutes = time.getMinutes();
+	secounds = time.getSeconds();
+	years = time.getFullYear();
+	months = time.getMonth()+1;
+	days = time.getDate();
+	/*hours = zeroPadding(hours);
+	minutes = zeroPadding(minutes);
+	secounds = zeroPadding(secounds);
+	months = zeroPadding(months);
+	days = zeroPadding(days);*/
+}
+function zeroPadding(zero){
+	if(zero < 0){
+		zero = '0' + zero;
+	}
+	return zero;
+}
+function judgment(){
+	var dates = new Date();
+	var backs;
+	if(dates.getFullYear() > years){
+		backs = years + '年' + months + '月' + days + '日';
+	}else if((dates.getMonth()+1 - months) > 0){
+		backs = years + '年' + months + '月' + days + '日'; 
+	}else if((dates.getDate() - days) > 0){
+		backs = (dates.getDate() - days) + '天前';
+	}else if((dates.getHours() - hours) > 0){
+		backs = (dates.getHours() - hours) + '小时前';
+	}else if((dates.getMinutes() - minutes) >= 1){
+		backs = (dates.getMinutes() - minutes) + '分钟前';
+	}else{
+		backs = '刚刚';
+	}
+	return backs;
+}
 Vue.component('time-list',{
 	props: ['list'],
 	template: '#time-list'
-})
-
+});
+Vue.directive('time',{
+	bind: function(el, binding) {
+		el._timeout_ = setInterval(function() {
+			el.innerHTML = judgment();
+		}, 60);
+		
+	},
+	unbind: function(el) {
+		clearInterval(el._timeout_);
+		delete el._timeout_;
+	}
+});
 var app = new Vue({
 	el: '#app',
 	data: {
@@ -24,7 +77,8 @@ var app = new Vue({
 	},
 	computed: {
 		thisTime: function () {
-			return (time);
+			getDays();
+			return (years + '年' + months + '月' + days + '日' + hours + ':' + minutes + ':' + secounds);
 		}
 	}
 })
